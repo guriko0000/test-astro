41e946f12c0c4ac1709088f3e39fac62246f4462
tagami <minominomino0121@gmail.com>
2025-01-20 10:49:21 +0900

    build動き修正用

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
 
-// https://astro.build/config
-export default defineConfig({});
+// .env ファイルを読み込む
+dotenv.config();
+
+export default defineConfig({
+  vite: {
+    define: {
+      // 環境変数を Vite に渡す
+      'process.env.MICROCMS_API_KEY': JSON.stringify(process.env.MICROCMS_API_KEY),
+      'process.env.MICROCMS_ENDPOINT': JSON.stringify(process.env.MICROCMS_ENDPOINT),
+    },
+  },
+});
+
